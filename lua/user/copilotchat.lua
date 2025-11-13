ca7e2b9c5ab93d39303dc1da5553ce5a3af11273
Jesper L. Nielsen <lyager@users.noreply.github.com>
2025-11-13 09:18:49 +0000

    feat(copilotchat): default mode is claude-sonnet-4.5

--- lua/user/copilotchat.lua
+++ lua/user/copilotchat.lua
@@ -1,13 +1,14 @@
 local status_ok, copilot_chat = pcall(require, "CopilotChat")
 if not status_ok then
-	return
+    return
 end
 
 local select = require("CopilotChat.select")
 --local chat = require("CopilotChat.chat")
 
 copilot_chat.setup({
-	debug = false,
-	trace = false,
-	selection = select.unnamed,
+    debug = false,
+    trace = false,
+    selection = select.unnamed,
+    model = 'claude-sonnet-4.5'
 })

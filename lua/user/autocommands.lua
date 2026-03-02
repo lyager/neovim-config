a8c11d435bc5bce54f50b3a23cd0bd3cb61d2192
Jesper L. Nielsen <lyager@users.noreply.github.com>
2026-03-02 10:35:40 +0100

    feat: add format-on-save toggle with FormatDisable/FormatEnable/FormatToggle commands
    Replace VimScript _lsp augroup with Lua autocommand that checks
    vim.g.disable_autoformat and vim.b.disable_autoformat guard variables.
    Supports :FormatDisable (global), :FormatDisable! (buffer-local),
    :FormatEnable, :FormatToggle, and :FormatToggle! commands.

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -31,9 +31,51 @@ vim.cmd([[
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
 
-  augroup _lsp
-   autocmd!
-   " Autoformat on write
-   autocmd BufWritePre * lua vim.lsp.buf.format()
- augroup end
 ]])
+
+-- Autoformat on write (with toggle support)
+-- Use :FormatDisable to disable, :FormatEnable to re-enable, :FormatToggle to toggle
+-- Use :FormatDisable! to disable only for the current buffer
+local lsp_format_augroup = vim.api.nvim_create_augroup("LspFormat", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = lsp_format_augroup,
+	callback = function(args)
+		if vim.g.disable_autoformat or vim.b[args.buf].disable_autoformat then
+			return
+		end
+		vim.lsp.buf.format({ bufnr = args.buf })
+	end,
+})
+
+vim.api.nvim_create_user_command("FormatDisable", function(opts)
+	if opts.bang then
+		-- :FormatDisable! disables for current buffer only
+		vim.b.disable_autoformat = true
+		vim.notify("Autoformat disabled for this buffer", vim.log.levels.INFO)
+	else
+		vim.g.disable_autoformat = true
+		vim.notify("Autoformat disabled globally", vim.log.levels.INFO)
+	end
+end, { desc = "Disable autoformat-on-save", bang = true })
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+	vim.notify("Autoformat enabled", vim.log.levels.INFO)
+end, { desc = "Enable autoformat-on-save" })
+
+vim.api.nvim_create_user_command("FormatToggle", function(opts)
+	if opts.bang then
+		vim.b.disable_autoformat = not vim.b.disable_autoformat
+		vim.notify(
+			"Autoformat " .. (vim.b.disable_autoformat and "disabled" or "enabled") .. " for this buffer",
+			vim.log.levels.INFO
+		)
+	else
+		vim.g.disable_autoformat = not vim.g.disable_autoformat
+		vim.notify(
+			"Autoformat " .. (vim.g.disable_autoformat and "disabled" or "enabled") .. " globally",
+			vim.log.levels.INFO
+		)
+	end
+end, { desc = "Toggle autoformat-on-save", bang = true })
